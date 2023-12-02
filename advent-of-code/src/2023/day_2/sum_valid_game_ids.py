808290724c8e8f779f42d2d9e0cb78a413af2741
nfarr <privat@noahfarr.com>
2023-12-02 21:35:24 +0100

    Implemented regex solution

--- advent-of-code/src/2023/day_2/sum_valid_game_ids.py
+++ advent-of-code/src/2023/day_2/sum_valid_game_ids.py
@@ -1,3 +1,5 @@
+import re, math
+
 N_COLORS = {
     "red": 12,
     "green": 13,
@@ -6,13 +8,18 @@ N_COLORS = {
 
 def main():
     lines = get_lines("/home/noah/Documents/advent-of-code/advent-of-code/src/2023/day_2/input.txt")
-    part_1(lines)
-    part_2(lines)
+    print(part_1(lines))
+    print(part_2(lines))
+    print(part_1_regex(lines))
+    print(part_2_regex(lines))
+
     
 def get_lines(path: str):
     with open(path) as f:
         lines = f.readlines()
     return lines
+
+# SOLUTION WITHOUT REGEX
     
 def part_1(lines: str):
     valid_ids = []
@@ -21,7 +28,7 @@ def part_1(lines: str):
         if is_valid_game(drawings):
             valid_ids.append(game_id)   
             
-    print(sum(valid_ids))
+    return sum(valid_ids)
     
 
 def part_2(lines: str):
@@ -35,8 +42,8 @@ def part_2(lines: str):
         red, green, blue = max_counts.values()
         powers.append(red * green * blue)
     
-    print(sum(powers))
-
+    return sum(powers)
+    
 def is_valid_game(drawings):
     return all(count <= N_COLORS[color] for count, color in drawings)
      
@@ -51,6 +58,13 @@ def parse_line(line):
     return int(game_id), drawings
             
                     
-            
+def part_1_regex(lines):
+    valid_ids = sum([game_id if all([int(count) <= N_COLORS[color] for count, color in re.findall("(\d+) ([a-zA-Z]*)", line)]) else 0 for game_id, line in enumerate(lines, 1)])
+    return valid_ids
+
+def part_2_regex(lines):
+    minimum_set_of_cubes = sum([math.prod([max([int(match) for match in re.findall(f"(\d+) {color}", line)]) for color in ["red", "green", "blue"]]) for line in lines])
+    return minimum_set_of_cubes
+    
 if __name__ == '__main__':
     main()
